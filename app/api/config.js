79cce69ed7289fed252b0ffa10222f3dc94db80d
veloc1tymeow <trashixx775@gmail.com>
2024-05-24 19:50:36 +0300

    fix 4

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://pindiefinal.nomoredomainswork.ru";
+export const BASE_URL = "https://pindiefinal.nomoredomainswork.ru/api";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
